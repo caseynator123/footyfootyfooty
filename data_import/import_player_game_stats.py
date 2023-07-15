0027a53b937ca22a2eb37856e83f48c95db49d31
caseynator123 <caseyhaseloff@gmail.com>
2023-07-15 16:16:29 +1000

    move to python files

---
+++ data_import/import_player_game_stats.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 17:21:07 2023
+
+@author: caseyh
+"""
+
+import os
+import pandas as pd
+import numpy as np
+
+os.getcwd()
+
+teams_str = ['adelaide', 'brisbanel','carlton', 'collingwood','essendon', 
+             'fremantle','geelong','goldcoast','gws', 'hawthorn', 'melbourne',
+             'kangaroos','padelaide','richmond','stkilda','swans','westcoast',
+             'bullldogs']
+
+statistics = ['Disposals',
+ 'Kicks',
+ 'Marks',
+ 'Handballs',
+ 'Goals',
+ 'Behinds',
+ 'Hit Outs',
+ 'Tackles',
+ 'Rebounds',
+ 'Inside 50s',
+ 'Clearances',
+ 'Clangers',
+ 'Frees',
+ 'Frees Against',
+ 'Contested Possessions',
+ 'Uncontested Possessions',
+ 'Contested Marks',
+ 'Marks Inside 50',
+ 'One Percenters',
+ 'Bounces',
+ 'Goal Assists',
+ '% Played',
+ 'Subs']
+
+opponent_mapping = {'RI':'RCH','GE':'GEE','NM':'NTH','PA':'POR','GC':'GCS','ME':'MEL','GW':'GWS','HW':'HAW','CA':'CAR','BL':'BRL','CW':'COL','AD':'ADE','FR':'FRE','WB':'WBD','FO':'WBD','SY':'SYD','WC':'WCE','SK':'STK','ES':'ESS'}
+team_mapping = {'richmond':'RCH','geelong':'GEE','kangaroos':'NTH','padelaide':'POR','goldcoast':'GCS','melbourne':'MEL','gws':'GWS','hawthorn':'HAW','carlton':'CAR','brisbanel':'BRL','collingwood':'COL','adelaide':'ADE','fremantle':'FRE','bullldogs':'WBD','swans':'SYD','westcoast':'WCE','stkilda':'STK','essendon':'ESS'}
+
+def rearrange(value):
+    return ' '.join(' '.join(value.lower().split(', ')[::-1]).split('-'))
+
+def clean(value):
+    if value == '-':
+        return 0
+    elif type(value) == float:
+        return np.nan
+    elif type(int(value)) == int:
+        return int(value)
+
+def get_data(year, team):
+    url = f'https://afltables.com/afl/stats/teams/{team}/{year}_gbg.html'
+    stat_tables = pd.read_html(url, attrs= {'class':'sortable'}, flavor = 'bs4')
+    try:
+        output = pd.DataFrame({})
+        
+        for table in stat_tables:
+            desc = table.columns[0][0]
+            df = table[desc]
+            
+            # get stats from tables
+            player_stats = df[(df['Player'] != 'Totals') & (df['Player'] != 'Opponent')].drop(columns = 'Tot')
+            
+            # flatten dataset into stat columns
+            player_stats = pd.melt(player_stats, id_vars=['Player']).rename(columns={'variable':'round','value':desc})
+            
+            # if empty then concat else merge with table
+            if output.shape[0] == 0:
+                output = pd.concat([output, player_stats])
+            else:
+                output = output.merge(player_stats, how = 'left', on = ['Player', 'round'])
+        
+        # get opponent team has played
+        opp_played = df[df['Player'] == 'Opponent'].melt(id_vars = 'Player').drop(columns='Player').rename(columns={'variable':'round','value':'opponent'})
+        output = output.merge(opp_played, how = 'left', on = ['round'])
+        
+        # add team and year to table
+        output['team'] = team
+        output['year'] = year
+        
+        output = output.fillna(np.nan)
+        
+        for col in output.columns:
+            try:
+                output[col] = output[col].apply(lambda x: clean(x))
+            except:
+                continue
+        
+        print(f'data collected: {year} {team}')
+        return output
+        
+    except:
+        print(f'no data: {year} {team}')
+        
+
+main = pd.DataFrame({})
+for team in teams_str:
+    for year in range(2023,2024):
+        
+        df = get_data(year, team)  
+        main = pd.concat([main, df])
+        
+        # change team names to 3 letter code
+        for key in opponent_mapping.keys():
+                main.loc[main['opponent']==key, 'opponent'] = opponent_mapping[key]
+        for key in team_mapping.keys():
+            main.loc[main['team']==key, 'team'] = team_mapping[key]
+        main['Player'] = main['Player'].apply(lambda x: rearrange(x))
+        
+        main.to_csv(f'/Users/caseyh/Desktop/footyfootyfooty/player_stats/{year}_player_stats.csv', index=False)
---
+++ data_import/sports_bet_odds.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 15 15:54:43 2023
+
+@author: caseyh
+"""
+
+import pandas as pd
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import re
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+
+disposals = ['To Get 15 or More Disposals',
+            'To Get 20 or More Disposals',
+            'To Get 25 or More Disposals',
+            'To Get 30 or More Disposals']
+team_mapping = {'richmond':'RCH','geelong':'GEE',
+                'north-melbourne':'NTH','port-adelaide':'POR',
+                'gold-coast':'GCS','melbourne':'MEL',
+                'greater-western-sydney':'GWS','hawthorn':'HAW',
+                'carlton':'CAR','brisbane':'BRL',
+                'collingwood':'COL','adelaide':'ADE',
+                'fremantle':'FRE','western-bulldogs':'WBD',
+                'sydney':'SYD','west-coast':'WCE',
+                'st-kilda':'STK','essendon':'ESS'}
+
+def get_odds(r):
+    url = f'https://www.sportsbet.com.au/betting/australian-rules/afl/round-{r}'
+    
+    day = datetime.now().strftime("%A").lower()
+    
+    soup = BeautifulSoup(requests.get(url).text, 'html.parser')
+    tmp = soup.find_all('script')
+    
+    game_urls = []
+    for i in tmp[:9]:
+        try:
+            url_find = re.compile(r'https://www.sportsbet.com.au/betting/australian-rules/afl/(.*?)\-\d{7}')
+            game_url = url_find.search(i.text)
+            game_urls.append(game_url.group())
+        except:
+            continue
+    
+    r_search = re.compile(r'\/(\w*\-?\w*\-?\w*)\-v\-(\w*\-?\w*\-?\w*)\-')
+    game_list = []
+    for game in game_urls:
+        game_list.append(r_search.findall(game))
+        
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.set_window_size(700,800)
+    
+    output = pd.DataFrame({},columns = {'teams','round','Player', 'disposals', 'odds'})
+    
+    for game, game_url in zip(game_list, game_urls):
+        driver.get(game_url)
+        try:
+            first_click = driver.find_element_by_xpath('(//*[@class="size14_f7opyze medium_f1wf24vo"])')
+            first_click.click()
+            
+            for i in range(1,7):
+                if driver.find_element_by_xpath(f'(//*[@class="size14_f7opyze medium_f1wf24vo"])[{i}]').text == 'Disposal Markets':
+                    disposal_market = driver.find_element_by_xpath(f'(//*[@class="size14_f7opyze medium_f1wf24vo"])[{i}]')
+            
+            
+            disposal_market.click()
+            time.sleep(3)
+            
+            for i in range(5,50):
+                try:
+                    tmp = driver.find_element_by_xpath(f'(//*[@class="size14_f7opyze bold_f1au7gae"])[{i}]').text
+                except:
+                    continue
+                if tmp in disposals:
+    
+                    dis = driver.find_element_by_xpath(f'(//*[@class="size14_f7opyze bold_f1au7gae"])[{i}]')
+    
+                    dis.click()
+    
+                    player = driver.find_element_by_xpath('(//*[@class="outcomeContainer_f1wc7xgg outcomeDetailsFirst_f162fqwy"])').text.split('\n')
+                    
+                    new_row = {'Player': player[0].lower(), 'teams': f'{game[0][0]} v {game[0][1]}', 'round':f'R{r}', 'disposals':tmp,'odds':player[-1]}
+                    output.loc[len(output)] = new_row
+    
+                    for j in range(1,50):
+                        try:
+    
+                            tmp1 = driver.find_element_by_xpath(f'(//*[@class="outcomeContainer_f1wc7xgg"])[{j}]').text.split('\n')
+                            
+                            new_row = {'Player': tmp1[0].lower(), 'teams': f'{game[0][0]} v {game[0][1]}', 'round':f'R{r}', 'disposals':tmp,'odds':tmp1[-1]}
+                            output.loc[len(output)] = new_row
+                        except:
+                            continue
+                    
+                    dis.click()
+                    time.sleep(2)
+                
+        except:
+            continue
+    
+    output['home'] = output['teams'].apply(lambda x: x.split(' v ')[0])
+    output['away'] = output['teams'].apply(lambda x: x.split(' v ')[1])
+    
+    for key in team_mapping.keys():
+         output.loc[output['home']==key, 'home'] = team_mapping[key]
+    for key in team_mapping.keys():
+        output.loc[output['away']==key, 'away'] = team_mapping[key]     
+        
+    output.to_csv(f'/Users/caseyh/Desktop/footyfootyfooty/sportsbet_odds/r{r}/disposal_markets_{day}.csv', index = False)
+
+get_odds(r='18')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
